18034a8a77f5ba494938b54d6ecb9c0ee16b6ce4
malathi416 <ammu.416@gmail.com>
2021-04-19 00:07:46 -0500

    Initial Commit with hello world

---
+++ src/test/java/com/malathi/schoolprojectdemo/SchoolProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malathi.schoolprojectdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
